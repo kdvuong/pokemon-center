dc5c35acb819d112a39156bdc906df70a0de7467
kdvuong <khangdieuvuong@gmail.com>
2021-01-04 13:39:27 -0600

    feat: implement learn type filter

--- src/utils/LearnTypeFilter.ts
+++ src/utils/LearnTypeFilter.ts
@@ -1,6 +1,27 @@
 import { $enum } from "ts-enum-util";
 import { LearnType } from "enums";
 
-export function getAllLearnTypes(): string[] {
-  return $enum(LearnType).getValues();
+export function getAllLearnTypeNames(): string[] {
+  return $enum(LearnType).getValues().map(getLearnTypeName);
+}
+
+function getLearnTypeName(learnType: LearnType): string {
+  return $enum.mapValue(learnType).with({
+    [LearnType.EGG]: "egg",
+    [LearnType.LEVEL_UP]: "level up",
+    [LearnType.MACHINE]: "machine",
+    [LearnType.TUTOR]: "tutor",
+  });
+}
+
+const learnTypeNameMap: Map<string, LearnType> = new Map<string, LearnType>();
+(function populateGenerationNameMap() {
+  $enum(LearnType).forEach((learnType) => {
+    const name = getLearnTypeName(learnType);
+    learnTypeNameMap.set(name, learnType);
+  });
+})();
+
+export function getLearnTypeFromName(name: string): LearnType {
+  return learnTypeNameMap.get(name) ?? LearnType.LEVEL_UP;
 }
