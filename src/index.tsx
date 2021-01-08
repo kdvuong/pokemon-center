ba1f089d2f742f5ecf0f882663cc3c77e9a0d406
kdvuong <khangdieuvuong@gmail.com>
2021-01-08 12:32:50 -0600

    feat: add onSuccess event handler for service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import "./icons.css";
 import App from "./App";
 import { routes } from "./router/config";
-import * as serviceWorker from "./serviceWorker";
+import { register, Config } from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -20,4 +20,15 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+
+const onSuccess = (registration: ServiceWorkerRegistration) => {
+  registration.addEventListener("fetch", (event) => {
+    console.log(event);
+  });
+};
+
+const config: Config = {
+  onSuccess,
+};
+
+register(config);
