a87075335d8d52cbe5e7bbb83c79392395b5e7cf
kdvuong <khangdieuvuong@gmail.com>
2021-01-12 22:16:00 -0600

    refactor: clean up

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 declare module "pouchdb";
 declare module "lodash-es";
 declare module "pouchdb-load";
+declare module "pouchdb-upsert";
 declare module "pokemonColors.json" {
   const value: Record<string, { background: string; content: string[] }>;
   export default value;
