894f81076e3f46ce8be9279cd251a6a7fdd85a6d
kdvuong <khangdieuvuong@gmail.com>
2021-01-18 12:12:41 -0600

    fix: matchup tab repeat already shown types

--- src/components/pokemon/detail/components/MatchupTab.tsx
+++ src/components/pokemon/detail/components/MatchupTab.tsx
@@ -19,13 +19,20 @@ const TypePillContainers = styled.div`
 const MatchupTab: FunctionComponent<IProps> = ({ types }) => {
   const { getTypesByIds } = usePokemonApi();
   const mapRef = useRef<Map<Type, number>>(new Map());
-  const weakAgainst = useRef<Type[]>([]);
-  const resistAgainst = useRef<Type[]>([]);
-  const normal = useRef<Type[]>([]);
+  const weakAgainstRef = useRef<Type[]>([]);
+  const resistAgainstRef = useRef<Type[]>([]);
+  const normalRef = useRef<Type[]>([]);
 
   useEffect(() => {
+    let isSubscribed: boolean = true;
     getTypesByIds(types.map((t) => t.id)).then((typeData) => {
-      const map = mapRef.current;
+      if (!isSubscribed) {
+        return;
+      }
+      const map: Map<Type, number> = new Map();
+      const weakAgainst: Type[] = [];
+      const resistAgainst: Type[] = [];
+      const normal: Type[] = [];
       $enum(Type)
         .getValues()
         .forEach((type) => {
@@ -49,40 +56,41 @@ const MatchupTab: FunctionComponent<IProps> = ({ types }) => {
             multiplier = 1;
           }
           if (multiplier > 1) {
-            weakAgainst.current.push(type);
+            weakAgainst.push(type);
           } else if (multiplier < 1) {
-            resistAgainst.current.push(type);
+            resistAgainst.push(type);
           } else {
-            normal.current.push(type);
+            normal.push(type);
           }
         });
+      mapRef.current = map;
+      weakAgainstRef.current = weakAgainst;
+      resistAgainstRef.current = resistAgainst;
+      normalRef.current = normal;
     });
     return () => {
-      mapRef.current = new Map();
-      weakAgainst.current = [];
-      resistAgainst.current = [];
-      normal.current = [];
+      isSubscribed = false;
     };
   }, [getTypesByIds, types]);
   return (
     <div className="container">
       <Section title={"Weak Against"}>
         <TypePillContainers>
-          {weakAgainst.current.map((type) => (
+          {weakAgainstRef.current.map((type) => (
             <TypeMultiplierPill type={type} multiplier={mapRef.current.get(type) ?? 1} />
           ))}
         </TypePillContainers>
       </Section>
       <Section title={"Resistant Against"}>
         <TypePillContainers>
-          {resistAgainst.current.map((type) => (
+          {resistAgainstRef.current.map((type) => (
             <TypeMultiplierPill type={type} multiplier={mapRef.current.get(type) ?? 1} />
           ))}
         </TypePillContainers>
       </Section>
       <Section title={"Normal Damage From"}>
         <TypePillContainers>
-          {normal.current.map((type) => (
+          {normalRef.current.map((type) => (
             <TypeMultiplierPill type={type} multiplier={mapRef.current.get(type) ?? 1} />
           ))}
         </TypePillContainers>
