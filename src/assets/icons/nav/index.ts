34ea9acc8b24e7f785e09103e208df3d36f5ee60
kdvuong <khangdieuvuong@gmail.com>
2021-01-17 22:11:22 -0600

    chore: add pikachu face icon

--- src/assets/icons/nav/index.ts
+++ src/assets/icons/nav/index.ts
@@ -1,9 +1,10 @@
 import Pokeball from "./pokeball.svg";
 import Search from "./search.svg";
-
+import PikachuFace from "./pikachu_face.svg";
 const icons = {
   Pokeball,
   Search,
+  PikachuFace,
 };
 
 export default icons;
