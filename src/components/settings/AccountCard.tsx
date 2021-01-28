da0098fb4e56955b8815af8b6ddbb33d555060c5
kdvuong <khangdieuvuong@gmail.com>
2021-01-27 19:38:22 -0600

    feat: implement and integrate edit modal

--- src/components/settings/AccountCard.tsx
+++ src/components/settings/AccountCard.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent, useContext } from "react";
 import { accountContext } from "contexts/AccountContext";
 import styled from "styled-components";
-import EditableField from "./EditableField";
+import UserField from "./UserField";
 
 const Card = styled.div`
   padding: 1rem;
@@ -33,48 +33,6 @@ const AccountCard: FunctionComponent = () => {
   const currentName = username?.name ?? "";
   const currentDiscriminator = username?.discriminator.toString() ?? "";
 
-  // const [name, setName] = useState<string>(currentName);
-  // const [discriminator, setDiscriminator] = useState<string>(currentDiscriminator);
-  // const [nameError, setNameError] = useState<string>("");
-  // const [discriminatorError, setDiscriminatorError] = useState<string>("");
-
-  // const handleNameChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  //   setNameError("");
-  //   setName(event.currentTarget.value);
-  // };
-
-  // const handleDiscriminatorChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-  //   setDiscriminatorError("");
-  //   setDiscriminator(event.currentTarget.value);
-  // };
-
-  // const handleClick = useCallback(async () => {
-  //   const isNameEmpty = name.length === 0;
-  //   const isDiscriminatorEmpty = discriminator.length === 0;
-
-  //   if (isNameEmpty) {
-  //     setNameError("Name cannot be empty");
-  //   }
-
-  //   if (isDiscriminatorEmpty) {
-  //     setDiscriminatorError("Discriminator cannot be empty");
-  //   }
-
-  //   if (isNameEmpty || isDiscriminatorEmpty) {
-  //     return;
-  //   }
-
-  //   try {
-  //     await updateUsername({ name, discriminator: parseInt(discriminator, 10) });
-  //   } catch (err) {
-  //     console.log(err);
-  //     if (err.statusCode === 409) {
-  //       setDiscriminator(currentDiscriminator);
-  //       setDiscriminatorError("Name and discriminator combination already in use");
-  //     }
-  //   }
-  // }, [currentDiscriminator, discriminator, name, updateUsername]);
-
   return (
     <Card>
       <Title>
@@ -82,31 +40,7 @@ const AccountCard: FunctionComponent = () => {
         <span>{`#${currentDiscriminator}`}</span>
       </Title>
       <AccountInfoCard>
-        <EditableField
-          name="Username"
-          value={`${currentName}#${currentDiscriminator}`}
-          onEdit={() => {}}
-          modalBody={<div>hello</div>}
-        />
-        {/* <TextField
-          id="filled-basic-name"
-          label="Name"
-          variant="filled"
-          onChange={handleNameChange}
-          error={nameError.length > 0}
-          helperText={nameError}
-          value={name}
-        />
-        <TextField
-          id="filled-basic-discriminator"
-          label="Discriminator"
-          variant="filled"
-          onChange={handleDiscriminatorChange}
-          error={discriminatorError.length > 0}
-          helperText={discriminatorError}
-          value={discriminator}
-        />
-        <button onClick={handleClick}>Submit</button> */}
+        <UserField />
       </AccountInfoCard>
     </Card>
   );
