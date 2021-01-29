c2056d5460252e986b90fdc7e0fc551337a2f56a
kdvuong <khangdieuvuong@gmail.com>
2021-01-28 18:06:32 -0600

    refactor: change network time out to 10s in production

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,11 @@ import axios, { AxiosRequestConfig } from "axios";
 
 const config: AxiosRequestConfig = {
   baseURL: process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3001/",
+<<<<<<< HEAD
   timeout: process.env.NODE_ENV === "production" ? 10000 : 5000,
+=======
+  timeout: process.env.NODE_ENV === "production" ? 10000 : 1000,
+>>>>>>> refactor: change network time out to 10s in production
   withCredentials: true,
   headers: {
     Accept: "application/json",
