e9324efd5309b2968c89308a1849ae2775487d25
kdvuong <khangdieuvuong@gmail.com>
2021-01-29 23:52:55 -0600

    fix: add retry on invalid/expired access token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 const config: AxiosRequestConfig = {
   baseURL: process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3001/",
@@ -11,4 +11,23 @@ const config: AxiosRequestConfig = {
 };
 
 const instance = axios.create(config);
+
+// retry to fetch access token
+instance.interceptors.response.use(undefined, (error: AxiosError) => {
+  // do not retry for any auth request
+  if (error.config.url?.match("auth")) {
+    return Promise.reject(error);
+  }
+  if (error.config && error.response?.status === 401) {
+    return instance.post("/auth/renew-token").then((res) => {
+      const AuthHeader = `Bearer ${res.data.accessToken}`;
+      instance.defaults.headers.common["Authorization"] = AuthHeader;
+      error.config.headers["Authorization"] = AuthHeader;
+      return axios.request(error.config);
+    });
+  }
+
+  return Promise.reject(error);
+});
+
 export { instance as axios };
